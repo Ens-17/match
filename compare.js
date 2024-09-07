559568225ba93501d6aa1cefd3d4ce610c60cd0d
Ens-17 <4ndgt.n.p@gmail.com>
2024-09-08 06:05:54 +0900

    Update compare.js

--- compare.js
+++ compare.js
@@ -91,3 +91,19 @@ function getDetailedComparison(text1, text2, file1Name, file2Name) {
 
     return result;
 }
+
+// スクロールして一定量下がったらボタンを表示
+window.onscroll = function() {
+    const btn = document.getElementById("scrollToTopBtn");
+    if (document.body.scrollTop > 300 || document.documentElement.scrollTop > 300) {
+        btn.style.display = "block";
+    } else {
+        btn.style.display = "none";
+    }
+};
+
+// トップにスクロールする関数
+function scrollToTop() {
+    window.scrollTo({top: 0, behavior: 'smooth'});
+}
+
